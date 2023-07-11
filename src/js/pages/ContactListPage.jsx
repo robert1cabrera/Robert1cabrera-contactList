4ac0bf9de3947795a116fa2234d062608fc8abc3
Robert <Robert1cabrera@gmail.com>
2023-07-11 01:22:47 +0000

    edited

--- src/js/pages/ContactListPage.jsx
+++ src/js/pages/ContactListPage.jsx
@@ -7,15 +7,15 @@ export default function ContactListPage() {
   const { state, dispatch } = useContext(Context);
 
   return (
-    <div>
+    <div className="cl-div">
       <h1 className="cl-h1">Contact List Page</h1>
-      
-      <button onClick={() => dispatch(contactListActions.ADD_CONTACT({ name: "Add something else" }))}class="btn-1 btn-primary">Add New Contact
-      </button>
+      <button type="button" class="btn-cl btn-success btn-md" onClick={() => dispatch(contactListActions.ADD_CONTACT({ name: "Add something else" }))}>Add New Contact</button>
       {state.contacts.map((contact) => (
-        <div key={contact.id}>
+        <div className="cl-div1" key={contact.id}>
           <p>{contact.name}</p>
+          <img src="https://placehold.co/100x100" className="cl-img" />
         </div>
+        
       ))}
     </div>
   );
